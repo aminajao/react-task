bad18a321931a1c3abd8c5d601a12139d15afe49
Bilal <39632030+aminajao@users.noreply.github.com>
2022-03-11 10:13:56 +0100

    what i didnt push

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,6 +1,45 @@
 import React from "react";
 import { Percentage } from "./Percentage";
 import Main from "./Main";
+import Icon1 from "../assets/icon-1.svg";
+import Icon2 from "../assets/icon-2.svg";
+import Icon3 from "../assets/icon-3.svg";
+
+const data = [
+  {
+    id: 1,
+    tag: "+5%",
+    tagType: "increase",
+    icon: Icon1,
+    heading: "Searches",
+    firstValue: 29380,
+    secondValue: 27985,
+    heading2: "Mobile traffic: 100%",
+    heading3: "Web traffic: 100%",
+    text: "You get 100% traffic on mobile and desktop devices.",
+  },
+  {
+    id: 2,
+    tag: "-13%",
+    tagType: "decrease",
+    icon: Icon2,
+    heading: "Clicks",
+    firstValue: 243,
+    secondValue: 280,
+    heading2: "CTR: 0,04%",
+    text: "Conversion from searches  to clicks on all devices.",
+  },
+  {
+    id: 3,
+    icon: Icon3,
+    heading: "Sales",
+    firstValue: 24,
+    secondValue: 24,
+    heading2: "STR: 6.2%",
+    heading3: "Avg. Check: 8 903₽",
+    text: "Conversion from cliks  to bookings on all devices.",
+  },
+];
 
 // const errorCode = {0: 1256, 1:}
 const Container = () => {
@@ -18,8 +57,10 @@ const Container = () => {
         <Percentage />
         <Percentage />
       </div>
-      <div>
-        <Main />
+      <div className="metrics">
+        {data?.map((eachData) => {
+          return <Main data={eachData} />;
+        })}
       </div>
     </div>
   );
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,30 +1,43 @@
 import React from "react";
 import Icon from "../assets/icon-1.svg";
 
-const Metric = () => {
+const Metric = ({ data }: any) => {
   return (
     <div className="metric--container">
       <div className="metric__icon--container">
-        <img src={Icon} alt="" />
+        <img src={data?.icon} alt="" />
       </div>
       <div className="metric__content--container">
         <div>
           <div className="flex">
-            <h2 className="metric__content--heading">Searches</h2>
-            <span className="tag">+5%</span>
+            <h2 className="metric__content--heading">{data?.heading}</h2>
+            {data?.tag ? (
+              <span
+                style={{
+                  background: `${
+                    data?.tagType === "increase" ? "#8bc34a" : "#FF6A67"
+                  }`,
+                }}
+                className="tag"
+              >
+                {data?.tag || ""}
+              </span>
+            ) : (
+              ""
+            )}
           </div>
           <div className="flex">
-            <p className="metric__content--key">29 380</p>
+            <p className="metric__content--key">{data?.firstValue}</p>
             <p className="metric__content--value">Yesterday</p>
           </div>
           <div className="flex">
-            <p className="metric__content--key2">27 985</p>
+            <p className="metric__content--key2">{data?.secondValue}</p>
             <p className="metric__content--value2">Last friday</p>
           </div>
         </div>
         <div>
-          <h2 className="metric__content--heading">Mobile traffic: 100%</h2>
-          <h2 className="metric__content--heading">Web traffic: 100%</h2>
+          <h2 className="metric__content--heading">{data?.heading2}</h2>
+          <h2 className="metric__content--heading">{data?.heading3}</h2>
           <p className="text__secondary">
             You get 100% traffic on mobile and desktop devices.
           </p>
